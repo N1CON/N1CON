9fdc3248d544f233925caa7d4088dff312d28303
yshin <Marzipam54647626>
2019-08-02 16:01:29 +0300

    New

---
+++ src/New.java
@@ -0,0 +1,9 @@
+public class New {
+    public static void main (String args[]) {
+        double var1 = 112.89;
+        double var2;
+        System.out.println("var1 = " + var1);
+            var2 = var1 * 5;
+        System.out.println("var2 = " +var2);
+    }
+}
